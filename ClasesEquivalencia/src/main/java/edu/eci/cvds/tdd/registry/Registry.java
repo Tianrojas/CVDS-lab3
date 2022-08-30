9d6e2afa362c5267ee5b7247d854ff118675ddba
TianRojas <62759668+Tianrojas@users.noreply.github.com>
2022-08-30 15:47:43 -0500

    v4

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,8 +1,33 @@
 package edu.eci.cvds.tdd.registry;
 
+import java.util.HashMap;
+
 public class Registry {
+	private HashMap<Integer, Person> voters;
+
+	public Registry() {
+		voters = new HashMap<Integer, Person>();
+	}
+
 	public RegisterResult registerVoter(Person p) {
-		// TODO Validate person and return real result.
-		return RegisterResult.VALID;
+		Integer id = p.getId();
+		boolean isAlive = p.isAlive();
+		int age = p.getAge();
+
+		RegisterResult registerResult = RegisterResult.VALID;
+
+		if (voters.containsKey(id)) {
+			registerResult = RegisterResult.DUPLICATED;
+		} else if (!isAlive) {
+			registerResult = RegisterResult.DEAD;
+		} else if (age < 0) {
+			registerResult = RegisterResult.INVALID_AGE;
+		} else if (age < 18) {
+			registerResult = RegisterResult.UNDERAGE;
+		} else {
+			voters.put(id, p);
+		}
+		return registerResult;
+
 	}
 }
--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -5,11 +5,55 @@ import org.junit.Test;
 
 public class RegistryTest {
 	private Registry registry = new Registry();
-
+	
 	@Test
-	public void validateRegistryResult() {
-		Person person = new Person();
-		RegisterResult result = registry.registerVoter(person);
-		Assert.assertEquals(RegisterResult.VALID, result);
-	}
+    public void should_dropVALID_when_AllTheParamsAreRigth() {
+		//arrange
+        Person person = new Person("Ana",32662323,24,Gender.UNIDENTIFIED,true);
+        //act
+        RegisterResult result = registry.registerVoter(person);
+        //assert
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+	
+    @Test
+    public void should_dropDUPLICATED_when_TheRegisterIsDuplicate() {
+    	//arrange
+        Person person = new Person("Diego",31672324,21,Gender.MALE,true);
+        //act
+        registry.registerVoter(person);
+        RegisterResult duplicateR = registry.registerVoter(person);
+        //assert
+        Assert.assertEquals(RegisterResult.DUPLICATED, duplicateR);
+    }
+
+    @Test
+    public void should_dropDEAD_when_ThePersonIsNotAlive() {
+    	//arrange
+        Person person = new Person("Victoria",12434723,36,Gender.FEMALE,false);
+        //act
+        RegisterResult result = registry.registerVoter(person);
+        //assert
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+
+    @Test
+    public void should_dropUNDERAGE_when_ThePersonIsUnder18() {
+    	//arrange
+        Person person = new Person("Tomas",94434723,12,Gender.UNIDENTIFIED,true);
+        //act
+        RegisterResult result = registry.registerVoter(person);
+        //assert
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    @Test
+    public void should_dropINVALID_AGE_when_ThePersonHasNotAValidCharAsItsAge() {
+    	//arrange
+        Person person = new Person("Diana",34334221,-13,Gender.FEMALE,true);
+        //act
+        RegisterResult result = registry.registerVoter(person);
+        //assert
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
 }
